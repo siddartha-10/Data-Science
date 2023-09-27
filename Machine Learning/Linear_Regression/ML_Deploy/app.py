853234ab6f3e3e8692551c99fd29ae93cbf0574f
Siddarth <pullakhandam.siddartha@gmail.com>
2023-09-26 23:03:52 -0500

    Update app.py

--- Machine Learning/Linear_Regression/ML_Deploy/app.py
+++ Machine Learning/Linear_Regression/ML_Deploy/app.py
@@ -6,7 +6,7 @@ import pickle
 
 app = Flask(__name__) # initializing a flask app
 
-@app.route('/',methods=['GET'])  # route to display the home page
+@app.route('/',methods=['GET'])  # route to display the home pages
 @cross_origin()
 def homePage():
     return render_template("index.html")
